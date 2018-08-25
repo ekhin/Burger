7108a90bf36c47f1ac532197228fa7cc76a8a891
ekhin <waikhin10@gmail.com>
2018-08-25 16:03:34 -0700

    edit

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,9 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-	host     : '',
-	user     : '',
+	host     : 'localhost',
+  port     : 3306,
+	user     : 'root',
 	password : '', 
 	database : 'burgers_db' 
 });
